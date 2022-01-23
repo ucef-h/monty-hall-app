35ba5ace2fc76c3df211e518abc4e268d10c3b11
ucefh <ucefh@live.com>
2022-01-23 21:55:54 +0200

    Fix Model

--- src/app/monty-app/game-result.model.ts
+++ src/app/monty-app/game-result.model.ts
@@ -1,5 +1,7 @@
+import { StringMap } from "@angular/compiler/src/compiler_facade_interface";
+
 export interface GameResultModel {
-  switchDoor: boolean;
+  playerStrategy: { id: StringMap,  name: string};
   playCount: number;
   winCount: number;
   probabilityOfWinning: number;
